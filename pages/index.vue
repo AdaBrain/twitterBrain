9a2e61d2d3de83d14758fee66feca256cc9220b1
adadesions <adadesions@gmail.com>
2022-01-29 14:39:56 +0700

    Add avatar

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="homepage container">
     <div class="row">
-      <div class="col-3 border-right">
+      <div class="col-3 col-lg-3 border-right">
         Left
       </div>
-      <div class="col-6 text-center">
-        <h1>Ada</h1>
+      <div class="col-6 col-md-6 col-lg-6">
+        <div class="tweet-box container border">
+          <div class="row">
+            <div class="col-3 border-right">
+              <img src="https://previews.123rf.com/images/pandavector/pandavector1704/pandavector170400314/75968328-avatar-of-a-man-in-a-shirt-avatar-and-face-single-icon-in-cartoon-style-vector-symbol-stock-illustra.jpg" class="img-thumbnail rounded mx-auto d-block" alt="">
+            </div>
+            <div class="col">
+              Text
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-3 border-left">
+      <div class="col-3 col-lg-3 border-left">
         Right
       </div>
-    </div>
-    <div class="row">
-      <h1>Bda</h1>
     </div>
   </div>
 </template>
