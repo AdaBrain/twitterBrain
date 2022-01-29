0c81d8f8e6a2a6910a798efad6abb6a09cbba6df
adadesions <adadesions@gmail.com>
2022-01-29 14:50:39 +0700

    Add textbox

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,15 @@
         <div class="tweet-box container border">
           <div class="row">
             <div class="col-3 border-right">
-              <img src="https://previews.123rf.com/images/pandavector/pandavector1704/pandavector170400314/75968328-avatar-of-a-man-in-a-shirt-avatar-and-face-single-icon-in-cartoon-style-vector-symbol-stock-illustra.jpg" class="img-thumbnail rounded mx-auto d-block" alt="">
+              <img src="https://previews.123rf.com/images/pandavector/pandavector1704/pandavector170400314/75968328-avatar-of-a-man-in-a-shirt-avatar-and-face-single-icon-in-cartoon-style-vector-symbol-stock-illustra.jpg" class="avatar img-thumbnail mx-auto d-block" alt="">
             </div>
             <div class="col">
-              Text
+              <div class="row">
+                <input class="tweet-input" type="text">
+              </div>
+              <div class="row">
+                button
+              </div>
             </div>
           </div>
         </div>
@@ -28,3 +33,15 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+  body {
+    background-color: black;
+  }
+
+  .avatar {
+    border-radius: 100%;
+    width: 3em;
+    height: auto;
+  }
+</style>
