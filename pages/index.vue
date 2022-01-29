cdf1bca682eae5d728a64916e1cae844856e9be1
adadesions <adadesions@gmail.com>
2022-01-29 14:24:26 +0700

    Add container

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="homepage">
-    <h1>welcome to TwitterBrain</h1>
+  <div class="homepage container">
+    <div class="row">
+      <div class="col-3 border-right">
+        Left
+      </div>
+      <div class="col-6 text-center">
+        <h1>Ada</h1>
+      </div>
+      <div class="col-3 border-left">
+        Right
+      </div>
+    </div>
+    <div class="row">
+      <h1>Bda</h1>
+    </div>
   </div>
 </template>
 
