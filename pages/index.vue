53ed8ffb3b0801b99ee18b7e3848a669eed1bbfa
adadesions <adadesions@gmail.com>
2022-01-29 15:12:59 +0700

    Before add script

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="homepage container">
     <div class="row">
-      <div class="col-3 col-lg-3 border-right">
-        Left
-      </div>
+      <div class="col-3 col-lg-3 border-right">Left</div>
       <div class="col-6 col-md-6 col-lg-6">
         <div class="tweet-box container">
           <div class="row">
             <div class="col-3">
-              <img src="https://previews.123rf.com/images/pandavector/pandavector1704/pandavector170400314/75968328-avatar-of-a-man-in-a-shirt-avatar-and-face-single-icon-in-cartoon-style-vector-symbol-stock-illustra.jpg" class="avatar img-thumbnail mx-auto d-block" alt="">
+              <img
+                src="https://previews.123rf.com/images/pandavector/pandavector1704/pandavector170400314/75968328-avatar-of-a-man-in-a-shirt-avatar-and-face-single-icon-in-cartoon-style-vector-symbol-stock-illustra.jpg"
+                class="avatar img-thumbnail mx-auto d-block"
+                alt=""
+              />
             </div>
             <div class="col">
               <div class="row">
                 <div class="col-12">
-                  <input class="tweet-input" type="text" placeholder="What's happening?">
+                  <input
+                    class="tweet-input"
+                    type="text"
+                    placeholder="What's happening?"
+                  />
                 </div>
               </div>
               <div class="row">
@@ -24,45 +30,53 @@
               </div>
             </div>
           </div>
+          <div class="row">
+            <div class="col">
+              <p><strong>Preview</strong></p>
+              <div class="preview-box border"></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="col-3 col-lg-3 border-left">
-        Right
-      </div>
+      <div class="col-3 col-lg-3 border-left">Right</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
 
 <style>
-  body {
-    background-color: black;
-  }
+body {
+  background-color: black;
+}
 
-  .avatar {
-    border-radius: 100%;
-    width: 3em;
-    height: auto;
-  }
+.avatar {
+  border-radius: 100%;
+  width: 3em;
+  height: auto;
+}
 
-  .tweet-box {
-    margin-top: 1em;
-  }
+.tweet-box {
+  margin-top: 1em;
+}
 
-  .tweet-input {
-    width: 100%;
-    background-color: black;
-    color: white;
-    border: none;
-    margin-bottom: 1em;
-  }
+.tweet-input {
+  width: 100%;
+  background-color: black;
+  color: white;
+  border: none;
+  margin-bottom: 1em;
+}
 
-  .tweet-input:focus {
-    outline: none;
-  }
+.tweet-input:focus {
+  outline: none;
+}
+
+.preview-box {
+  color: white;
+}
 </style>
