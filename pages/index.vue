d855eb36de8948462d2699e4cb710ca4d0a0766a
adadesions <adadesions@gmail.com>
2022-02-05 14:44:04 +0700

    Add tweetpost to index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-3 col-lg-3 border-right">Left</div>
       <div class="col-6 col-md-6 col-lg-6">
-
+        <tweet-post></tweet-post>
       </div>
       <div class="col-3 col-lg-3 border-left">Right</div>
     </div>
@@ -11,8 +11,13 @@
 </template>
 
 <script>
+import TweetPost from "../components/TweetPost.vue"
+
 export default {
   name: "IndexPage",
+  components: {
+    TweetPost,
+  }
 };
 </script>
 
